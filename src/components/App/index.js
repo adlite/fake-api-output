ba719a0c3c9a85811a6379044a166b5d911ccbcf
Adlite <adlite@yandex.ru>
2018-07-07 15:55:37 +0300

    Add typeface-roboto

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import 'typeface-roboto';
 // Routes
 import routes from '../../routes';
 import NotFound from '../../pages/NotFound';
