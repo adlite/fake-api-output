f5390f35b4a9240e44e68e39ac53b7979847fbad
igor.sebelev <igor.sebelev@chulakov.ru>
2018-07-09 18:59:52 +0300

    Create NotFound page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,6 +1,12 @@
 // Vendor
 import React from 'react';
+import Layout from '../../components/Layout';
+import Fetcher from '../../components/Fetcher';
 
-const NotFound = () => <h1>Page not found</h1>;
+const NotFound = () => (
+  <Layout title="Not found">
+    <Fetcher isLoading={false} error="Page not found" />
+  </Layout>
+);
 
 export default NotFound;
