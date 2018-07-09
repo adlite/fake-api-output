ffe8b58362bbcbb4d467f8cd73ff03c52d0c79c0
igor.sebelev <igor.sebelev@chulakov.ru>
2018-07-09 17:02:50 +0300

    Configure redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 // Vendor
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+const middleware = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
+
+export default createStore(rootReducer, applyMiddleware(...middleware));
