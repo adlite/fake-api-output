77612427a490cf9ebc7c7b3d32bb5106a0df3574
Adlite <adlite@yandex.ru>
2018-07-09 08:00:13 +0300

    Configure transform-decorators and connect Homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,16 @@
 // Vendor
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 // Components
 import Layout from '../../components/Layout';
+// Actions
+import * as actions from '../../store/modules/posts';
 
+@connect()
 class Home extends PureComponent {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.dispatch(actions.postsFetch());
+  }
 
   render() {
     return <Layout>Hello World</Layout>;
